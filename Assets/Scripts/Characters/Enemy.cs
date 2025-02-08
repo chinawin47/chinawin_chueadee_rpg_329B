94bab0fcde1cc14eaaa695cd81bdea2d0377895d
chinawin47 <chinawin47@gmail.com>
2025-02-08 12:28:59 +0700

    Code NavAgent

--- Assets/Scripts/Characters/Enemy.cs
+++ Assets/Scripts/Characters/Enemy.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class Enemy : Characters
 {
- 
+ void Update()
+    {
+        switch(state)
+        {
+            case CharState.Walk:
+                WalkUpdate();
+                break;
+        }
+    }
 }
--- Assets/Scripts/Characters/Hero.cs
+++ Assets/Scripts/Characters/Hero.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class Hero : Characters
 {
-   
+    void Update()
+    {
+        switch (state)
+        {
+            case CharState.Walk:
+                WalkUpdate();
+                break;
+        }
+    }
 }
