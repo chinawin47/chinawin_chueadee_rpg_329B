82b1e93f624c150ed95598ccca884650e9e4e23d
chinawin47 <chinawin47@gmail.com>
2025-02-08 13:28:21 +0700

    Right click

--- Assets/Scripts/Command/RightClick.cs
+++ Assets/Scripts/Command/RightClick.cs
@@ -2,15 +2,53 @@ using UnityEngine;
 
 public class RightClick : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private Camera cam;
+    public LayerMask layerMask;
+
+        private LeftClick leftClick;
+    public static RightClick instance;
+
+   void Awake()
     {
-        
+        leftClick = GetComponent<LeftClick>();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        instance = this;
+        cam = Camera.main;
+        layerMask = LayerMask.GetMask("Ground", "Character", "Building");
+    }
+
     void Update()
     {
-        
+        if (Input.GetMouseButtonUp(1))
+        {
+            TryCommand(Input.mousePosition);
+        }
     }
+
+    private void CommandToWalk(RaycastHit hit, Characters c)
+    {
+        if (c != null)
+            c.WalkToPosition(hit.point);
+    }
+
+    private void TryCommand(Vector2 ScreenPos)
+    {
+        Ray ray = cam.ScreenPointToRay(ScreenPos);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1000, layerMask))
+        { 
+          switch (hit.collider.tag)
+            {
+                case "Ground":
+                    CommandToWalk(hit, leftClick.CurChar);
+                    break;
+            }
+        }
+    }
+
 }
