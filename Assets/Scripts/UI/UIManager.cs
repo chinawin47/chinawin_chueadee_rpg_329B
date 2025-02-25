cba009c3c84dfdef8bba726b6752808cfcb6e9fb
chinawin47 <chinawin47@gmail.com>
2025-02-25 19:48:26 +0700

    Complet 14

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private RectTransform selectionBox;
-    public RectTransform SelectionBox { get { return selectionBox; } } 
+    public RectTransform SelectionBox { get { return selectionBox; } }
+
+    [SerializeField]
+    private Toggle togglePauseUnpause;
 
     public static UIManager instance;
 
@@ -11,4 +15,43 @@ public class UIManager : MonoBehaviour
     {
         instance = this;
     }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space)) 
+        togglePauseUnpause.isOn = !togglePauseUnpause.isOn;
+    }
+
+
+    public void ToggleAI(bool isOn)
+    {
+        foreach (Character member in PartyManager.instance.Members)
+        {
+            AttackAI ai = member.gameObject.GetComponent<AttackAI>();
+
+            if (ai != null)
+                ai.enabled = isOn;
+        }
+    }
+
+    public void SelectAll()
+    {
+        foreach (Character member in PartyManager.instance.Members)
+        {
+          if(member.CurHP > 0)
+            {
+                member.ToggleRingSelection(true);
+                PartyManager.instance.SelectChars.Add(member);
+            }
+        }
+    }
+
+    public void PauseUnpause(bool isOn)
+    {
+        Time.timeScale = isOn ? 0 : 1;
+    }
+
+
+
+
 }
